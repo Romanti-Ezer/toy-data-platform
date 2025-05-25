e1fe94c36464f8f16178f95f85a189155bd126c4
lorransr <lorran.rodr@gmail.com>
2025-05-25 10:05:40 -0300

    feat: add pyiceberg

--- dlt/filesystem_pipeline.py
+++ dlt/filesystem_pipeline.py
@@ -1,27 +1,41 @@
 import dlt
 import argparse
-from dlt.sources.filesystem import filesystem, read_csv
+import pandas as pd
+from utils.iceberg import create_or_update_iceberg_table, get_generated_parquet_files
+
 
 def stream_csv(table_name):
-    data = filesystem(file_glob=f"{table_name}.csv") | read_csv()
-    data.apply_hints(table_name=table_name)
-    return data
+    return pd.read_csv(f"data/{table_name}.csv")
 
-def main(table_name):
+
+def main(table_name, table_catalog="raw", table_schema="movies"):
     pipeline = dlt.pipeline(
-        pipeline_name="standard_filesystem_csv",
+        pipeline_name="iceberg_filesystem_csv",
         destination="filesystem",
-        dataset_name="movies",
+        dataset_name=f"raw_{table_schema}",
+        progress="log",
     )
 
-    load_info = pipeline.run(stream_csv(table_name),table_format="iceberg",write_disposition="append")
+    load_info = pipeline.run(
+        stream_csv(table_name), write_disposition="append", table_name=table_name
+    )
+
+    parquet_paths = get_generated_parquet_files(pipeline, table_name)
+    print(f"generated_files: {parquet_paths}")
+    create_or_update_iceberg_table(
+        table_catalog, table_schema, table_name, parquet_paths
+    )
 
     print(load_info)
     print(pipeline.last_trace.last_normalize_info)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process CSV files into raw layer.")
-    parser.add_argument("-d", "--dataset", type=str, help="Name of the dataset", required=True)
-    args = parser.parse_args()
 
-    main(args.dataset)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Process CSV files into Iceberg tables."
+    )
+    parser.add_argument(
+        "-t", "--table", type=str, help="Name of the table", required=True
+    )
+    args = parser.parse_args()
+    main(args.table)
